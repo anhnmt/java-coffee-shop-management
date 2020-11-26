d256f81725e05dd84ef2d04715d28bab87da34da
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2020-11-26 15:07:10 +0700

    Update FrmLogin, ...

--- src/main/java/com/example/GUI/FrmLogin.java
+++ src/main/java/com/example/GUI/FrmLogin.java
@@ -5,12 +5,9 @@
  */
 package com.example.GUI;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.Arrays;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -26,10 +23,12 @@ public class FrmLogin extends javax.swing.JFrame {
         setLocationRelativeTo(null);
 
         // Custom code
+        lblBackground.setBounds(0, 0, 960, 610);
+        lblBackground.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/com/example/assets/img/background.png")).getImage().getScaledInstance(lblBackground.getWidth(), lblBackground.getHeight(), 1)));
+
         lblEmailError.setVisible(false);
         lblPasswordError.setVisible(false);
 
-        lblBackground.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/com/example/assets/img/background-2.png")).getImage().getScaledInstance(lblBackground.getWidth(), lblBackground.getHeight(), 1)));
         txtEmail.setBorder(BorderFactory.createCompoundBorder(
                 txtEmail.getBorder(),
                 BorderFactory.createEmptyBorder(5, 8, 5, 8)));
@@ -37,6 +36,116 @@ public class FrmLogin extends javax.swing.JFrame {
                 txtPassword.getBorder(),
                 BorderFactory.createEmptyBorder(5, 8, 5, 8)));
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrmLogin().setVisible(true);
+            }
+        });
+    }
+
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        // TODO add your handling code here:
+        String email = txtEmail.getText().trim();
+        String password = String.valueOf(txtPassword.getPassword()).trim();
+        boolean validate = true;
+
+        // Reset to default
+        lblEmailError.setVisible(false);
+        lblPasswordError.setVisible(false);
+        txtEmail.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(new Color(34, 36, 40)),
+                BorderFactory.createEmptyBorder(5, 8, 5, 8)));
+        txtPassword.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(new Color(34, 36, 40)),
+                BorderFactory.createEmptyBorder(5, 8, 5, 8)));
+        lblEmail.setForeground(new Color(220, 221, 222));
+        lblPassword.setForeground(new Color(220, 221, 222));
+
+        if (email.equals("")) {
+            txtEmail.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(new Color(240, 71, 71)),
+                    BorderFactory.createEmptyBorder(5, 8, 5, 8)));
+            lblEmail.setForeground(new Color(240, 71, 71));
+            lblEmailError.setText("Email không được để trống");
+            lblEmailError.setVisible(true);
+            validate = false;
+        } else {
+            String email_regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+            if (!email.matches(email_regex)) {
+                txtEmail.setBorder(BorderFactory.createCompoundBorder(
+                        BorderFactory.createLineBorder(new Color(240, 71, 71)),
+                        BorderFactory.createEmptyBorder(5, 8, 5, 8)));
+                lblEmail.setForeground(new Color(240, 71, 71));
+                lblEmailError.setText("Email không đúng định dạng");
+                lblEmailError.setVisible(true);
+                validate = false;
+            }
+        }
+
+        if (password.equals("")) {
+            txtPassword.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(new Color(240, 71, 71)),
+                    BorderFactory.createEmptyBorder(5, 8, 5, 8)));
+            lblPassword.setForeground(new Color(240, 71, 71));
+            lblPasswordError.setText("Mật khẩu không được để trống");
+            lblPasswordError.setVisible(true);
+            validate = false;
+        }
+
+        if (validate == true) {
+            JOptionPane.showMessageDialog(null, "Đăng nhập thành công!");
+        }
+    }//GEN-LAST:event_btnLoginActionPerformed
+
+    private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            btnLogin.doClick();
+        }
+    }//GEN-LAST:event_txtPasswordKeyPressed
+
+    private void lblForgotPasswordMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotPasswordMouseClicked
+        JOptionPane.showMessageDialog(null, "Vui lòng liên hệ admin để lấy lại mật khẩu.");
+    }//GEN-LAST:event_lblForgotPasswordMouseClicked
+
+    private void lblRegisterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegisterMouseClicked
+        JOptionPane.showMessageDialog(null, "Vui lòng liên hệ admin để đăng ký tài khoản.");        // TODO add your handling code here:
+    }//GEN-LAST:event_lblRegisterMouseClicked
+
+    private void txtEmailKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEmailKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            btnLogin.doClick();
+        }
+    }//GEN-LAST:event_txtEmailKeyPressed
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -49,7 +158,6 @@ public class FrmLogin extends javax.swing.JFrame {
 
         panelBackround = new javax.swing.JPanel();
         lblTitle = new javax.swing.JLabel();
-        lblBackground = new javax.swing.JLabel();
         panelLogin = new javax.swing.JPanel();
         boxLogin = new javax.swing.JPanel();
         lblEmail = new javax.swing.JLabel();
@@ -64,28 +172,25 @@ public class FrmLogin extends javax.swing.JFrame {
         lblEmailError = new javax.swing.JLabel();
         lblPassword = new javax.swing.JLabel();
         lblPasswordError = new javax.swing.JLabel();
+        lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Quản Lý Cửa Hàng Cafe - Version 1.0");
-        setMaximumSize(new java.awt.Dimension(960, 610));
         setMinimumSize(new java.awt.Dimension(960, 610));
         setResizable(false);
+        setSize(new java.awt.Dimension(960, 610));
 
         panelBackround.setMaximumSize(new java.awt.Dimension(960, 610));
         panelBackround.setMinimumSize(new java.awt.Dimension(960, 610));
         panelBackround.setPreferredSize(new java.awt.Dimension(960, 610));
+        panelBackround.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         lblTitle.setBackground(new java.awt.Color(255, 255, 255));
         lblTitle.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         lblTitle.setForeground(new java.awt.Color(255, 255, 255));
         lblTitle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblTitle.setText("QUẢN LÝ CỬA HÀNG CAFE");
-
-        lblBackground.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblBackground.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        lblBackground.setMaximumSize(new java.awt.Dimension(960, 610));
-        lblBackground.setMinimumSize(new java.awt.Dimension(960, 610));
-        lblBackground.setPreferredSize(new java.awt.Dimension(960, 610));
+        panelBackround.add(lblTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 960, -1));
 
         panelLogin.setBackground(new java.awt.Color(54, 57, 63));
         panelLogin.setMaximumSize(new java.awt.Dimension(480, 406));
@@ -244,159 +349,38 @@ public class FrmLogin extends javax.swing.JFrame {
             .addGroup(panelLoginLayout.createSequentialGroup()
                 .addContainerGap(32, Short.MAX_VALUE)
                 .addComponent(boxLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(32, Short.MAX_VALUE))
+                    .addContainerGap(32, Short.MAX_VALUE))
         );
         panelLoginLayout.setVerticalGroup(
-            panelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelLoginLayout.createSequentialGroup()
-                .addContainerGap(32, Short.MAX_VALUE)
-                .addComponent(boxLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(32, Short.MAX_VALUE))
+                panelLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(panelLoginLayout.createSequentialGroup()
+                                .addContainerGap(32, Short.MAX_VALUE)
+                                .addComponent(boxLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(32, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout panelBackroundLayout = new javax.swing.GroupLayout(panelBackround);
-        panelBackround.setLayout(panelBackroundLayout);
-        panelBackroundLayout.setHorizontalGroup(
-            panelBackroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lblTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 960, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addGroup(panelBackroundLayout.createSequentialGroup()
-                .addGap(220, 220, 220)
-                .addComponent(panelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addComponent(lblBackground, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        panelBackroundLayout.setVerticalGroup(
-            panelBackroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelBackroundLayout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(lblTitle)
-                .addGap(58, 58, 58)
-                .addComponent(panelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addComponent(lblBackground, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
+        panelBackround.add(panelLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 100, -1, -1));
+
+        lblBackground.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblBackground.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblBackground.setMaximumSize(new java.awt.Dimension(960, 610));
+        lblBackground.setMinimumSize(new java.awt.Dimension(960, 610));
+        lblBackground.setPreferredSize(new java.awt.Dimension(960, 610));
+        panelBackround.add(lblBackground, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 610));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelBackround, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(panelBackround, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelBackround, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(panelBackround, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
-        // TODO add your handling code here:
-        String email = txtEmail.getText().trim();
-        String password = String.valueOf(txtPassword.getPassword()).trim();
-        boolean validate = true;
-
-        // Reset to default
-        lblEmailError.setVisible(false);
-        lblPasswordError.setVisible(false);
-        txtEmail.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createLineBorder(new Color(34, 36, 40)),
-                BorderFactory.createEmptyBorder(5, 8, 5, 8)));
-        txtPassword.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createLineBorder(new Color(34, 36, 40)),
-                BorderFactory.createEmptyBorder(5, 8, 5, 8)));
-        lblEmail.setForeground(new Color(220, 221, 222));
-        lblPassword.setForeground(new Color(220, 221, 222));
-
-        if (email.equals("")) {
-            txtEmail.setBorder(BorderFactory.createCompoundBorder(
-                    BorderFactory.createLineBorder(new Color(240, 71, 71)),
-                    BorderFactory.createEmptyBorder(5, 8, 5, 8)));
-            lblEmail.setForeground(new Color(240, 71, 71));
-            lblEmailError.setText("Email không được để trống");
-            lblEmailError.setVisible(true);
-            validate = false;
-        } else {
-            String email_regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-            if (!email.matches(email_regex)) {
-                txtEmail.setBorder(BorderFactory.createCompoundBorder(
-                        BorderFactory.createLineBorder(new Color(240, 71, 71)),
-                        BorderFactory.createEmptyBorder(5, 8, 5, 8)));
-                lblEmail.setForeground(new Color(240, 71, 71));
-                lblEmailError.setText("Email không đúng định dạng");
-                lblEmailError.setVisible(true);
-                validate = false;
-            }
-        }
-
-        if (password.equals("")) {
-            txtPassword.setBorder(BorderFactory.createCompoundBorder(
-                    BorderFactory.createLineBorder(new Color(240, 71, 71)),
-                    BorderFactory.createEmptyBorder(5, 8, 5, 8)));
-            lblPassword.setForeground(new Color(240, 71, 71));
-            lblPasswordError.setText("Mật khẩu không được để trống");
-            lblPasswordError.setVisible(true);
-            validate = false;
-        }
-
-        if (validate == true) {
-            JOptionPane.showMessageDialog(null, "Đăng nhập thành công!");
-        }
-    }//GEN-LAST:event_btnLoginActionPerformed
-
-    private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            btnLogin.doClick();
-        }
-    }//GEN-LAST:event_txtPasswordKeyPressed
-
-    private void lblForgotPasswordMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgotPasswordMouseClicked
-        JOptionPane.showMessageDialog(null, "Vui lòng liên hệ admin để lấy lại mật khẩu.");
-    }//GEN-LAST:event_lblForgotPasswordMouseClicked
-
-    private void lblRegisterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblRegisterMouseClicked
-        JOptionPane.showMessageDialog(null, "Vui lòng liên hệ admin để đăng ký tài khoản.");        // TODO add your handling code here:
-    }//GEN-LAST:event_lblRegisterMouseClicked
-
-    private void txtEmailKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtEmailKeyPressed
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            btnLogin.doClick();
-        }
-    }//GEN-LAST:event_txtEmailKeyPressed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FrmLogin().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel boxLogin;
